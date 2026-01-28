a8e2580bd9d825426e49dd654acc1c260ab548c5
IMTISAL HUSSAIN <imtisalhussan@gmail.com>
2026-01-28 18:08:52 +0500

    nave-bar-fixing

--- app/about/page.js
+++ app/about/page.js
@@ -1,9 +1,12 @@
 "use client";
 
+import Navbar from "@/app/components/navebar"
+
 
 export default function About (){
     return(
-        <div>
+        <div className="bg-slate-900 min-h-screen">
+            <Navbar />
             <h1>about</h1>
         </div>
     )
